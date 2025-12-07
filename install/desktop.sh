adb71464e6bc1bd8717ec96ea11048bba10fec79
JuanraCM <juanracm.dev@gmail.com>
2025-12-07 18:01:32 +0000

    feat(niri): add niri config files and config-refresh-niri script

--- install/desktop.sh
+++ install/desktop.sh
@@ -2,3 +2,7 @@
 sudo pacman -Syu --noconfirm niri xwayland-satellite xdg-desktop-portal-gnome xdg-desktop-portal-gtk alacritty
 paru -S --noconfirm dms-shell-bin matugen wl-clipboard cliphist cava qt6-multimedia-ffmpeg
 systemctl --user add-wants niri.service dms
+
+# Copy Niri config files
+mkdir -p $HOME/.config/niri
+cp -r $DEFAULT_PATH/niri/* $HOME/.config/niri/
