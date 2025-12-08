0e1c8f8bb4b13bab7f924e2cc6b4c6c85c0cdd6b
JuanraCM <juanracm.dev@gmail.com>
2025-12-08 16:00:29 +0000

    feat(limine): add limine config

--- install/config.sh
+++ install/config.sh
@@ -23,3 +23,42 @@ sudo systemctl enable bluetooth.service
 
 # Set default shell
 chsh -s /bin/zsh
+
+# Setup limine
+if [[ -f /boot/EFI/BOOT/limine.conf ]]; then
+  ## USB location
+  limine_config="/boot/EFI/BOOT/limine.conf"
+else
+  ## Default location
+  limine_config="/boot/EFI/limine/limine.conf"
+fi
+
+if [[ ! -f $limine_config ]]; then
+  echo "Error: Limine config not found at $limine_config" >&2
+  exit 1
+fi
+
+CMDLINE=$(cat /proc/cmdline)
+  sudo tee /etc/default/limine <<EOF >/dev/null
+TARGET_OS_NAME="Arch Linux"
+
+ESP_PATH="/boot"
+
+KERNEL_CMDLINE[default]="$CMDLINE"
+KERNEL_CMDLINE[default]+="quiet splash"
+
+ENABLE_UKI=yes
+CUSTOM_UKI_NAME="arch"
+
+ENABLE_LIMINE_FALLBACK=yes
+
+FIND_BOOTLOADERS=yes
+
+BOOT_ORDER="*, *fallback, Snapshots"
+ENABLE_SORT=no
+EOF
+
+sudo cp $DEFAULT_PATH/limine/limine.conf /boot/limine.conf
+sudo rm "$limine_config"
+
+sudo limine-update
