5f1a791b906fdaef4e973efed65d1d3eb8740f42
JuanraCM <juanracm.dev@gmail.com>
2025-12-12 16:02:06 +0000

    feat(webapps): add webapps

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ export CONFIG_PATH="$HOME/.local/share/arch-config"
 export CONFIG_INSTALL="$CONFIG_PATH/install"
 export CONFIG_DEFAULT="$CONFIG_PATH/default"
 export CONFIG_INSTALL_LOG_FILE="/var/log/arch-config-install.log"
+export PATH="$CONFIG_PATH/bin:$PATH"
 
 # Install
 source "$CONFIG_INSTALL/helpers/all.sh"
@@ -19,7 +20,7 @@ run_logged "$CONFIG_INSTALL/preflight.sh"
 run_logged "$CONFIG_INSTALL/packaging.sh"
 run_logged "$CONFIG_INSTALL/config.sh"
 run_logged "$CONFIG_INSTALL/desktop.sh"
-run_logged "$CONFIG_INSTALL/postflight.sh"
+source "$CONFIG_INSTALL/postflight.sh"
 
 stop_install_log
 show_header "Installation Complete! Reboot when ready."
--- install/postflight.sh
+++ install/postflight.sh
@@ -1,2 +1,6 @@
 # Remove sudo-less access
 sudo rm -f /etc/sudoers.d/install-mode
+
+# Install webapps
+config-webapp-install DeepL https://www.deepl.com/es/translator https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/deepl.png
+config-webapp-install YouTube https://www.youtube.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
