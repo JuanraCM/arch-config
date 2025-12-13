9af169163a2701dfd7e96c86f589920ff0302a1d
JuanraCM <juanracm.dev@gmail.com>
2025-12-13 21:28:35 +0000

    feat(webapps): install WhatsApp

--- install/postflight.sh
+++ install/postflight.sh
@@ -4,3 +4,4 @@ sudo rm -f /etc/sudoers.d/install-mode
 # Install webapps
 config-webapp-install DeepL https://www.deepl.com/es/translator https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/deepl.png
 config-webapp-install YouTube https://www.youtube.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
+config-webapp-install WhatsApp https://web.whatsapp.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/whatsapp.png
