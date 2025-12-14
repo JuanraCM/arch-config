61f7abe624e4d44e860093b560fc81b19f4fc521
JuanraCM <juanracm.dev@gmail.com>
2025-12-14 19:59:11 +0000

    feat(aur): replace paru with yay

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,6 +1,6 @@
 # Niri install script: https://yalter.github.io/niri/Getting-Started.html
 sudo pacman -Syu --noconfirm niri xwayland-satellite xdg-desktop-portal-gnome xdg-desktop-portal-gtk alacritty
-paru -S --noconfirm dms-shell-bin greetd-dms-greeter-git matugen wl-clipboard cliphist cava qt6-multimedia-ffmpeg
+yay -S --noconfirm dms-shell-bin greetd-dms-greeter-git matugen wl-clipboard cliphist cava qt6-multimedia-ffmpeg
 systemctl --user add-wants niri.service dms
 
 # Enable dms greeter
--- install/packaging.sh
+++ install/packaging.sh
@@ -4,17 +4,17 @@ sudo pacman -Syu --noconfirm
 # Install base dependencies
 sudo pacman -S --needed --noconfirm base-devel
 
-# Install paru
-git clone https://aur.archlinux.org/paru-bin.git
-cd paru-bin
+# Install yay
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
 makepkg -si --noconfirm
 cd ..
-rm -rf paru-bin
+rm -rf yay-bin
 
 # Install pacman packages
 mapfile -t packages < <(grep -v '^#' "$CONFIG_INSTALL/pacman.packages" | grep -v '^$')
 sudo pacman -S --noconfirm --needed "${packages[@]}"
 
 # Install paru packages
-mapfile -t packages < <(grep -v '^#' "$CONFIG_INSTALL/paru.packages" | grep -v '^$')
-paru -S --noconfirm --needed "${packages[@]}"
+mapfile -t packages < <(grep -v '^#' "$CONFIG_INSTALL/aur.packages" | grep -v '^$')
+yay -S --noconfirm --needed "${packages[@]}"
